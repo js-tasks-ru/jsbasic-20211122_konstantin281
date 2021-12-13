f4b124920feb2df64052e64974a38505474206d5
Полуянов Константин <poluyano@yandex.ru>
2021-12-13 20:04:24 +0300

    Добавил задачи с личным проектом и кнопками

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,5 +1,5 @@
 function toggleText() {
-  document.addEventListener('click', () => {
+  document.addEventListener('click', function () {
     elem = document.getElementById('text');
     elem.hidden = !elem.hidden;
   });
